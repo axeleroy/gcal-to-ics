cdad992194c415b162aac04c55ac322ba50b9609
Axel Leroy <axeleroy@users.noreply.github.com>
2025-12-15 15:21:50 +0100

    Parsing date

--- src/icalendar.test.ts
+++ src/icalendar.test.ts
@@ -1,25 +1,49 @@
 // @vitest-environment jsdom
 
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 import { buildICalendar, getDates } from "./icalendar";
+import dayjs from "dayjs";
 
 describe("buildIcalendar", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  })
+
+  afterEach(() => {
+    vi.useRealTimers();
+    vi.unstubAllGlobals();
+  })
+
   it('should return a valid iCalendar event', () => {
     // GIVEN
     const searchParams = new URLSearchParams("?action=TEMPLATE&text=Birthday&dates=20201231T193000Z/20201231T223000Z&details=With%20clowns%20and%20stuff&location=North%20Pole");
+    vi.setSystemTime(new Date("2025-12-15T00:00:00.000Z"));
+    vi.stubGlobal("crypto", {
+      randomUUID : vi.fn(() => "foo")
+    });
 
     // WHEN
     const result = buildICalendar(searchParams);
 
     // THEN
-    expect(result).toBe(null)
-
+    expect(result.ok).toBe(true);
+    expect(result.ok && result.result).toBe(`BEGIN:VEVENT\r
+UID:foo\r
+SEQUENCE:0\r
+DTSTAMP:20251215T000000Z\r
+DTSTART:20201231T193000Z\r
+DTEND:20201231T223000Z\r
+SUMMARY:Birthday\r
+LOCATION:North Pole\r
+DESCRIPTION:With clowns and stuff\r
+END:VEVENT\r
+`)
   });
+  // TODO: test all other cases
 })
 
-// TODO: parse dates into Date objects using dayjs
 describe('getDates', () => {
-  it('should parse full date times', () => {
+  it('should parse full date times in UTC', () => {
     // GIVEN
     const searchParams = new URLSearchParams("dates=20201231T193000Z/20201231T223000Z");
 
@@ -29,13 +53,29 @@ describe('getDates', () => {
     // THEN
     expect(res.ok).toBe(true);
     expect(res.ok && res.result).toEqual({
-      start: "",
-      end: ""
+      start: dayjs("2020-12-31T19:30:00Z"),
+      end: dayjs("2020-12-31T22:30:00Z"),
+      allDay: false
+    })
+  });
+  it('should parse full date times in local timezone', () => {
+    // GIVEN
+    const searchParams = new URLSearchParams("dates=20201231T193000/20201231T223000");
+
+    // WHEN
+    const res = getDates(searchParams);
+
+    // THEN
+    expect(res.ok).toBe(true);
+    expect(res.ok && res.result).toEqual({
+      start: dayjs("2020-12-31T19:30:00"),
+      end: dayjs("2020-12-31T22:30:00"),
+      allDay: false
     })
   });
   it('should parse dates only', () => {
     // GIVEN
-    const searchParams = new URLSearchParams("dates=20201231T193000Z/20201231T223000Z");
+    const searchParams = new URLSearchParams("dates=20201231Z/20201231Z");
 
     // WHEN
     const res = getDates(searchParams);
@@ -43,8 +83,9 @@ describe('getDates', () => {
     // THEN
     expect(res.ok).toBe(true);
     expect(res.ok && res.result).toEqual({
-      start: "",
-      end: ""
+      start: dayjs("2020-12-31"),
+      end: dayjs("2020-12-31"),
+      allDay: true
     })
   });
   it('should return a failure if dates is missing', () => {
--- src/icalendar.ts
+++ src/icalendar.ts
@@ -2,18 +2,25 @@ import { failure, Result, success } from "./types";
 import { safeGetSearchParam } from "./utils";
 import ical, { ICalAttendeeData, ICalEventBusyStatus, ICalEventData, ICalEventTransparency } from "ical-generator";
 import { match } from "ts-pattern";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import UTC from "dayjs/plugin/utc";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(UTC);
 
 export function buildICalendar(searchParams: URLSearchParams): Result<string> {
   const dates = getDates(searchParams);
   if (!dates.ok) {
     return failure();
   }
-  const {start, end} = dates.result
+  const {start, end, allDay} = dates.result
   const id = self.crypto.randomUUID();
   const event: ICalEventData = {
     id,
     start,
     end,
+    allDay,
     summary: safeGetSearchParam(searchParams,"text"),
     description: searchParams.get("details"),
     location: searchParams.get("location"),
@@ -26,19 +33,40 @@ export function buildICalendar(searchParams: URLSearchParams): Result<string> {
   return success(ical().createEvent(event).toString());
 }
 
-export function getDates(searchParams: URLSearchParams): Result<{ start: string, end: string}> {
+const allDayFormat = "YYYYMMDD";
+const localTimeFormat = `${allDayFormat}THHmmss`;
+const utcTimeFormat = `${localTimeFormat}Z`;
+
+export function getDates(searchParams: URLSearchParams): Result<{ start: dayjs.Dayjs, end: dayjs.Dayjs, allDay: boolean}> {
   if (!searchParams.has("dates")) {
     console.error('Search params do not contain "dates" entry', searchParams);
     return failure();
   }
 
   const dates = searchParams.get("dates")!;
-  const [start, end] = dates.split("/");
-  if (!start || !end) {
+  const [startStr, endStr] = dates.split("/");
+  if (!startStr || !endStr) {
     console.error(`One of the dates are missing from dates search param: ${dates}`)
-    return failure()
+    return failure();
   }
-  return success({ start, end });
+  const allDay = !dates.includes("T");
+  const isUtc = dates.includes("Z");
+  let formatToUse: string;
+  if (allDay) {
+    formatToUse = allDayFormat;
+  } else if (isUtc) {
+    formatToUse = utcTimeFormat;
+  } else {
+    formatToUse = localTimeFormat;
+  }
+
+  const start = dayjs(startStr, formatToUse);
+  const end = dayjs(endStr, formatToUse);
+  if (!start.isValid() || !end.isValid()) {
+    console.error(`One of the dates is invalid: ${dates}`)
+    return failure();
+  }
+  return success({ start, end, allDay });
 }
 
 export function getBusyStatus(searchParams: URLSearchParams): ICalEventBusyStatus | null {
