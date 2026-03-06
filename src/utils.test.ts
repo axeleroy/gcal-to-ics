5a139ed7c965a269ac3d9e54a1d63c72ec1bfab6
Axel Leroy <axeleroy@users.noreply.github.com>
2026-03-06 14:47:16 +0100

    chore: test utils

---
+++ src/utils.test.ts
@@ -0,0 +1,73 @@
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
+import { extractSearchParams, safeGetSearchParam } from "./utils";
+
+describe("extractSearchParams", () => {
+    it("should return search params when URL contains them", () => {
+        // GIVEN
+        const url = "https://example.com/url?foo=bar&baz=buzz";
+
+        // WHEN
+        const result = extractSearchParams(url);
+
+        // THEN
+        expect(result.ok).toBe(true);
+        expect(result.ok && result.result).toEqual(
+            new URLSearchParams({
+                foo: "bar",
+                baz: "buzz",
+            }),
+        );
+    });
+
+    it("should return empty URLSearchParams if the URL does not contain any params", () => {
+        // GIVEN
+        const url = "https://example.com/url";
+
+        // WHEN
+        const result = extractSearchParams(url);
+
+        // THEN
+        expect(result.ok).toBe(true);
+        expect(result.ok && result.result).toEqual(new URLSearchParams());
+    });
+
+    it("should return a failure if the URL is invalid", () => {
+        // GIVEN
+        const url = "/foo";
+
+        // WHEN
+        const result = extractSearchParams(url);
+
+        // THEN
+        expect(result.ok).toBe(false);
+    });
+});
+
+describe("safeGetSearchParam", () => {
+    it("should return the value if key exists", () => {
+        // GIVEN
+        const params = new URLSearchParams({
+            foo: "bar",
+            baz: "buzz",
+        });
+
+        // WHEN
+        const result = safeGetSearchParam(params, "foo");
+
+        // THEN
+        expect(result).toEqual("bar");
+    });
+    it("should return empty string if key does not exist", () => {
+        // GIVEN
+        const params = new URLSearchParams({
+            foo: "bar",
+            baz: "buzz",
+        });
+
+        // WHEN
+        const result = safeGetSearchParam(params, "bar");
+
+        // THEN
+        expect(result).toEqual("");
+    });
+});
