eb97b9c16d8801e5fe99584b6d8c29b7a30af2f5
Axel Leroy <axeleroy@users.noreply.github.com>
2026-03-06 14:51:34 +0100

    fix: lint errors

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { extractSearchParams, safeGetSearchParam } from "./utils";
 
 describe("extractSearchParams", () => {
